40e069804d194c24a2e7ac6ecc31f5e9e48f9b59
Vitor A <vitorabsebold@outlook.com>
2023-05-14 17:30:12 -0300

    update with backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+   const [backendData, setBackendData] = useState([{}])
+
+   useEffect(() => {
+    fetch('http://localhost:5100/api').then(
+      resp => resp.json()
+    ).then(
+      data => setBackendData(data)
+    ).catch(
+      e => console.log(e)
+    )
+   }, [])
+
+   console.log(backendData);
 
   return (
-    <h1>Hello</h1>
+    <div></div>
   )
 }
 
