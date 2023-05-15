401696a740a54c7fd45304a1deeab37453afdb16
Vitor A B Sebold <vitorabsebold@outlook.com>
2023-05-15 13:38:49 -0300

    Update

--- server/server.js
+++ server/server.js
@@ -1,22 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const qs = require("querystring");
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api", (req, res) => {
-  res.json({ user: ["userOne", "userTwo", "userThree"] });
-});
-
 app.post("/validateLogin", (req, res) => {
-  if (req.body.username == "Vitor" && req.body.password == "vitor") {
-    console.log(req.body.username);
-    console.log(req.body.password);
-    res.redirect("http://localhost:5173/");
+  if (req.body.username === "Vitor" && req.body.password === "vitor") {
+    res.redirect("http://localhost:5173/acessopermitido");
   }
+  res.redirect("http://localhost:5173/acessonegado");
 });
 
 app.listen("3000", () => {
