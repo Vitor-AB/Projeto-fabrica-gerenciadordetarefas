90ce2ce9749e205cfdbbe3b1dea899609cfa1abb
Vitor A B Sebold <vitorabsebold@outlook.com>
2023-05-15 10:46:50 -0300

    Update

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,22 +1,19 @@
-import styles from "./LoginForm.module.css"
+import styles from "./LoginForm.module.css";
 
 function LoginForm(params) {
-
-
-
-    return (
-            <div className={styles.login_container}>
-                <div className={styles.form_container}>
-                    <form action="http://localhost:5100/validateLogin" method="post">
-                        <label htmlFor="username">Username</label>
-                        <input type="text" name="username" id="username" />
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="password" />
-                        <button type="submit">Logar</button>
-                    </form>
-                </div>
-            </div>
-        )
+  return (
+    <div className={styles.login_container}>
+      <div className={styles.form_container}>
+        <form action="http://localhost:3000/validateLogin" method="post">
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" id="username" />
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+          <button type="submit">Logar</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default LoginForm;
--- server/server.js
+++ server/server.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser')
-const qs = require('querystring');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const qs = require("querystring");
 const app = express();
 
-app.use(cors())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api", (req,res) => {
-    res.json({"user": ["userOne", "userTwo","userThree"]})
-})
+app.get("/api", (req, res) => {
+  res.json({ user: ["userOne", "userTwo", "userThree"] });
+});
 
-app.post("/validateLogin", (req,res) => {
-    if(req.body.username == "Vitor" && req.body.password == "vitor"){
-        console.log(req.body.username);
-        console.log(req.body.password);
-        res.redirect("http://localhost:5173/")
-    }
-})
+app.post("/validateLogin", (req, res) => {
+  if (req.body.username == "Vitor" && req.body.password == "vitor") {
+    console.log(req.body.username);
+    console.log(req.body.password);
+    res.redirect("http://localhost:5173/");
+  }
+});
 
-
-app.listen(5100, () => {console.log("Server started on port 5100")})
+app.listen("3000", () => {
+  console.log("Server started on port 3000");
+});
