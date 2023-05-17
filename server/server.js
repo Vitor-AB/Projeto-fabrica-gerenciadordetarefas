a2e4e596f0be5e9152e672fff3fb07f3f971354d
Vitor A B Sebold <vitorabsebold@outlook.com>
2023-05-17 11:41:46 -0300

    update db

--- server/server.js
+++ server/server.js
@@ -1,34 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const { sql } = require("./Connection");
+const { getUserPass } = require("./db-manipulation");
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/validateLogin", (req, res) => {
-  if (req.body.username === "Vitor" && req.body.password === "vitor") {
-    res.redirect("http://localhost:5173/acessopermitido");
+  async function validateLogin(name) {
+    const user = await getUserPass(name);
+    console.log(user);
+    if (user.length !== 0) {
+      const { username } = user[0];
+      const { password } = user[0];
+      if (req.body.username === username && req.body.password === password) {
+        res.redirect("http://localhost:5173/acessopermitido");
+      } else {
+        res.redirect("http://localhost:5173/acessonegado");
+      }
+    } else {
+      res.redirect("http://localhost:5173/acessonegado");
+    }
   }
-  res.redirect("http://localhost:5173/acessonegado");
+
+  validateLogin(req.body.username);
 });
 
 app.listen("3000", () => {
   console.log("Server started on port 3000");
 });
-
-async function createTable() {
-  const table = await sql`
-  CREATE TABLE IF NOT EXISTS valid_users( username TEXT , password TEXT )
-  `;
-}
-
-async function insertTable() {
-  const insert = await sql`
-  INSERT INTO valid_users ( username , password )
-  VALUES ( 'User' , '1234' )
-  `;
-}
-
-insertTable();
