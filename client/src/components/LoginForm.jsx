4dbb1495b698dc9078659e9fee90b6d6ce742227
Vitor A B Sebold <vitorabsebold@outlook.com>
2023-05-18 09:54:51 -0300

    Update tela login css

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -4,13 +4,16 @@ function LoginForm() {
   return (
     <div className={styles.login_container}>
       <div className={styles.form_container}>
-        <form action="http://localhost:3000/validateLogin" method="post">
-          <label htmlFor="username">Username</label>
-          <input type="text" name="username" id="username" />
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" />
-          <button type="submit">Logar</button>
-        </form>
+        <div className={styles.input_container}>
+          <form action="http://localhost:3000/validateLogin" method="post">
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" id="username" />
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" />
+
+            <button type="submit">Logar</button>
+          </form>
+        </div>
       </div>
     </div>
   );
