e54c6f57927cf17335d760229ffff99c07865ed6
VITOR ANTONIO BONIN SEBOLD <vitor.sebold@univille.br>
2023-05-26 19:26:11 -0300

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import styles from "./App.module.css";
-import LoginForm from "./components/LoginForm";
-import AcessoNegado from "./components/AcessNegado";
+import LoginForm from "./components/LoginECadastro/LoginForm";
+import AcessoNegado from "./components/LoginECadastro/AcessoNegado";
 import GerenciadorDeTarefas from "./components/GerenciadorDeTarefas";
-import Cadastro from "./components/Cadastro";
+import Cadastro from "./components/LoginECadastro/Cadastro";
 
 function App() {
   return (
--- client/src/components/LoginECadastro/AcessoNegado.jsx
+++ client/src/components/LoginECadastro/AcessoNegado.jsx
@@ -1,4 +1,4 @@
-export default function AcessNegado() {
+export default function AcessoNegado() {
   return (
     <div>
       <h1>Acesso Negado!</h1>
