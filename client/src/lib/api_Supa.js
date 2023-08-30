8739a7f49503d0b40513b0555e84396037025801
Vitor AB <88466135+Vitor-AB@users.noreply.github.com>
2023-08-30 10:06:24 -0300

    Update api_Supa.js

--- client/src/lib/api_Supa.js
+++ client/src/lib/api_Supa.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://zfkjduqqqghwsyspbbka.supabase.co';
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpma2pkdXFxcWdod3N5c3BiYmthIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQzMjg0MTIsImV4cCI6MTk5OTkwNDQxMn0.siZR-w0KyMjS4TP9JfqdUKczon4LIfquFYAs9PoW3-Q";
+const supabaseUrl = YOUR_SUPABASE_URL;
+const supabaseKey = YOUR_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
